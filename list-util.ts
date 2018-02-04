25e001594c75974d29ee6480f6742e964ee1e66d
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-02-04 14:01:32 +0100

    Adjust exports

--- list-util.ts
+++ list-util.ts
@@ -1,15 +1,9 @@
-import {isNot, sameAs, bigger, smaller, includedIn, differentFrom, times} from './src/list-util-base';
-export {isNot, sameAs, bigger, smaller, includedIn, differentFrom, times};
-import {take, takeRightWhile, dropWhile, takeUntil, takeWhile} from './src/list-util-drop-take';
-export {take, takeRightWhile, dropWhile, takeUntil, takeWhile};
-import {flow, reverse, map, filter} from './src/list-util-flow';
-export {flow, reverse, map, filter};
-import {getAtIndex, getAtIndexOr, removeAtIndex} from './src/list-util-index';
-export {getAtIndex, getAtIndexOr, removeAtIndex};
-import {NestedArray, intersection, union, intersect, subtract, unite} from './src/list-util-sets';
-export {NestedArray, intersection, union, intersect, subtract, unite};
-import {addUniqueTo, removeFrom} from './src/list-util-extras';
-export {addUniqueTo, removeFrom};
+export {isNot, sameAs, bigger, smaller, includedIn, differentFrom, times} from './src/list-util-base';
+export {take, takeRightWhile, dropWhile, takeUntil, takeWhile} from './src/list-util-drop-take';
+export {flow, reverse, map, filter} from './src/list-util-flow';
+export {getAtIndex, getAtIndexOr, removeAtIndex} from './src/list-util-index';
+export {NestedArray, intersection, union, intersect, subtract, unite} from './src/list-util-sets';
+export {addUniqueTo, removeFrom} from './src/list-util-extras';
 
 /**
  * @author Daniel de Oliveira
