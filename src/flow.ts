311c00c7ad5f28da8fa4ce53cab51181873056f2
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2018-02-15 20:42:42 +0000

    Generalize flow and flowP

--- src/flow.ts
+++ src/flow.ts
@@ -9,13 +9,13 @@ type Transformation<A> = (_: Array<A>) => Array<A>;
  * A flow is a transformation from Array of A to Array of A, consisting
  * of n transformation steps, where n >= 0.
  */
-export const flow = <A>(collection: Array<A>, ...transformations: Array<Transformation<A>>) =>
-    transformations.reduce((acc, transformation) => transformation(acc), collection);
+export const flow = <T>(t: T, ...transformations: Array<(_: T) => T>) =>
+    transformations.reduce((acc, transformation) => transformation(acc), t);
 
 
-export const flowP = <A>(...transformations: Array<Transformation<A>>) =>
-    (collection: Array<A>) =>
-        transformations.reduce((acc, transformation) => transformation(acc), collection);
+export const flowP = <T>(...transformations: Array<(_: T) => T>) =>
+    (t: T) =>
+        transformations.reduce((acc, transformation) => transformation(acc), t);
 
 
 // These are special versions of the javascript array methods that work within
