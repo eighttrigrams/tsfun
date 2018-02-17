6ca41b37207c783398a36bb0a010443295c76ea9
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2018-02-17 03:16:40 +0000

    Simplify

--- src/sets-o.ts
+++ src/sets-o.ts
@@ -2,23 +2,23 @@
  * @author Daniel de Oliveira
  */
 import {intersect, subtract} from "./sets";
-import {identical, reduceO} from "./core";
+import {identical, reduceO, obj} from "./core";
 
 export const subtractO = (subtrahend: Array<string|number>|any) =>
-    (o: {[prop: string]: any}): {[prop: string]: any} => {
+    (o: obj): obj => {
 
         if (Array.isArray(o)) throw new TypeError('invalid argument');
 
-        const result = Array.isArray(subtrahend)
-            ? reduceO(identical)(subtract(subtrahend.map(_ => _.toString()))(Object.keys(o)), o)
-            : reduceO(identical)(subtract(Object.keys(subtrahend))(Object.keys(o)), o);
-
-        return (Array.isArray(o)) ? Object.values(result) : result;
+        return reduceO(identical)(
+            Array.isArray(subtrahend)
+                ? (subtract(subtrahend.map(_ => _.toString()))(Object.keys(o)))
+                : (subtract(Object.keys(subtrahend))(Object.keys(o)))
+            , o);
     };
 
 
 export const uniteO = (addend: Array<string|number>|any) =>
-    (o: {[prop: string]: any}): {[prop: string]: any} => {
+    (o: obj): obj => {
 
         if (Array.isArray(addend)
             || Array.isArray(o)) throw new TypeError('invalid argument');
@@ -28,16 +28,18 @@ export const uniteO = (addend: Array<string|number>|any) =>
 
 
 export const intersectO = (o1: any) =>
-    (o2: {[prop: string]: any}): {[prop: string]: any} => {
+    (o2: obj): obj => {
 
         if (Array.isArray(o2)) throw new TypeError('invalid argument');
 
-        return reduceO(identical)(
-            intersect(
+        const intersectingKeys = intersect(
                 Array.isArray(o1)
                     ? o1.map(x => x.toString())
                     : Object.keys(o1)
-            )(Object.keys(o2)),
+            )(Object.keys(o2));
+
+        return reduceO(identical)(
+            intersectingKeys,
             (!Array.isArray(o1) ? o1 : o2)
         );
     };
