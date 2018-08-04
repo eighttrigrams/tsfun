4197214fde82f350d51f7e987e4f3f28f46b6476
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-04 03:33:10 +0200

    Fix formatting

--- test/maps/set_like.spec.ts
+++ test/maps/set_like.spec.ts
@@ -9,116 +9,102 @@ export function main() {
 
     describe('Maps/Set-Like-Collection', () => {
 
-        it('subtract - subtract by array of numeric keys', () =>
+        it('subtractMap - subtract by array of numeric keys', () =>
             expect(subtractMap([1])({1: 3, 2: 4}))
                 .toEqual({2: 4}));
 
 
-        it('subtract - subtract by array of string keys', () =>
+        it('subtractMap - subtract by array of string keys', () =>
             expect(subtractMap(['1'])({1: 3, 2: 4}))
                 .toEqual({2: 4}));
 
 
-        it('subtract - subtract objects', () =>
+        it('subtractMap - subtract objects', () =>
             expect(subtractMap({1: 7})({1: 3, 2: 4}))
                 .toEqual({2: 4}));
 
 
-        it('subtract - array for o', () =>
+        it('subtractMap - array for o', () =>
             expect(() => subtractMap({0: 7})([2, 4]))
                 .toThrow(new TypeError('invalid argument')));
 
 
-        it('subtract - retain instance', () => {
+        it('subtractMap - retain instance', () => {
             const instance = { a: 'hey'  };
-            expect(
-                subtractMap({1: 7})({1: 3, 2: instance})['2']
-            ).toBe(instance);
+            expect(subtractMap({1: 7})({1: 3, 2: instance})['2'])
+                .toBe(instance);
         });
 
 
-        it('unite - overwrite', () =>
+        it('uniteMap - overwrite', () =>
             expect(uniteMap({1: 4})({1: 3, 2: 4}))
                 .toEqual({1: 4, 2: 4}));
 
 
-        it('unite - unite different', () => {
-            expect(
-                uniteMap({1: 4})({2: 4})
-            ).toEqual({1: 4, 2: 4});
-        });
+        it('uniteMap - unite different', () =>
+            expect(uniteMap({1: 4})({2: 4}))
+                .toEqual({1: 4, 2: 4}));
 
 
-        it('unite - illegal first arg', () => {
-            expect(
-                () => uniteMap([])({2: 4})
-            ).toThrow(new TypeError('invalid argument'));
-        });
+        it('uniteMap - illegal first arg', () =>
+            expect(() => uniteMap([])({2: 4}))
+                .toThrow(new TypeError('invalid argument')));
 
 
-        it('unite - illegal second arg', () => {
-            expect(
-                () => uniteMap({1: 4})([])
-            ).toThrow(new TypeError('invalid argument'));
-        });
+        it('uniteMap - illegal second arg', () =>
+            expect(() => uniteMap({1: 4})([]))
+                .toThrow(new TypeError('invalid argument')));
 
 
-        it('unite - retain instance', () => {
+        it('uniteMap - retain instance', () => {
             const instance = { a: 'hey'  };
-            expect(
-                uniteMap({1: 4})({1: 3, 2: instance})[2]
-            ).toBe(instance);
+            expect(uniteMap({1: 4})({1: 3, 2: instance})[2])
+                .toBe(instance);
         });
 
 
-        it('unite - variadic', () =>
-            expect(
-                uniteMap(...[{3: 4}, {4: 4}])({1: 2})
-            ).toEqual({1: 2, 3: 4, 4: 4}));
+        it('uniteMap - variadic', () =>
+            expect(uniteMap(...[{3: 4}, {4: 4}])({1: 2}))
+                .toEqual({1: 2, 3: 4, 4: 4}));
 
 
-        it('unite - variadic', () =>
-            expect(
-                uniteMap(...[{3: 4}, {4: 4}])({1: 2})
-            ).toEqual({1: 2, 3: 4, 4: 4}));
+        it('uniteMap - variadic', () =>
+            expect(uniteMap(...[{3: 4}, {4: 4}])({1: 2}))
+                .toEqual({1: 2, 3: 4, 4: 4}));
 
 
-        it('union', () =>
-            expect(
-                unionMap([{3: 4}, {4: 4}, {1: 2}])
-            ).toEqual({1: 2, 3: 4, 4: 4})
-        );
+        it('unionMap', () =>
+            expect(unionMap([{3: 4}, {4: 4}, {1: 2}]))
+                .toEqual({1: 2, 3: 4, 4: 4}));
 
 
-        it('union - one', () =>
-            expect(
-                unionMap([{3: 4}])
-            ).toEqual({3: 4})
-        );
+        it('unionMap - one', () =>
+            expect(unionMap([{3: 4}]))
+                .toEqual({3: 4}));
 
 
-        it('union - empty', () =>
+        it('unionMap - empty', () =>
             expect(unionMap([]))
                 .toEqual({}));
 
 
-        it('intersect', () =>
+        it('intersectMap', () =>
             expect(intersectMap({1: 4})({1: 3, 2: 4}))
                 .toEqual({1: 4}));
 
 
-        it('intersect - array', () =>
+        it('intersectMap - array', () =>
             expect(intersectMap([1])({1: 3, 2: 4}))
                     .toEqual({1: 3}));
 
 
-        it('intersect - array for o', () =>
+        it('intersect Map- array for o', () =>
             expect(
                 () => intersectMap({1: 3, 2: 4})([1]))
                 .toThrow(new TypeError('invalid argument')));
 
 
-        it('intersect - retain instance', () => {
+        it('intersectMap - retain instance', () => {
             const instance = { a: 'hey'  };
             expect(
                 intersectMap({1: instance})({1: instance, 2: 4})[1]
