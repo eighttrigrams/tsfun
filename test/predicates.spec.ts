f2fb021c1f9c231f307578a0cdf2a6eb50c8186a
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-10 23:20:20 +0200

    Change formatting

--- test/predicates.spec.ts
+++ test/predicates.spec.ts
@@ -8,25 +8,42 @@ export function main() {
     describe('Drop', () => {
 
         it('even', () =>
-            expect(isEven(4)).toEqual(true));
+            expect(
+
+                isEven(4))
+
+                .toEqual(true));
 
 
         it('even - 0', () =>
-            expect(isEven(0))
+            expect(
+
+                isEven(0))
+
                 .toEqual(true));
 
 
         it('even - -2', () =>
-            expect(isEven(0))
+            expect(
+
+                isEven(0))
+
                 .toEqual(true));
 
 
         it('odd', () =>
-            expect(isOdd(7))
+            expect(
+
+                isOdd(7))
+
                 .toEqual(true));
 
 
         it('odd - -1', () =>
-            expect(isOdd(-1)).toEqual(true))
+            expect(
+
+                isOdd(-1))
+
+                .toEqual(true))
     })
 }
