20fc501571f6c8fa89be0d69d9bac59e7eb11e85
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-11 17:30:09 +0200

    Use const form and use typing

--- src/maps/list_like.ts
+++ src/maps/list_like.ts
@@ -1,17 +1,18 @@
 import {mapProperties} from './core';
-import {TypedMap} from '../types';
+import {Predicate, Transformation, TypedMap} from '../types';
 
 
-export function mapMap<A, B>(f: (_: A) => B): (_: TypedMap<A>) => TypedMap<B> {
+export const mapMap = <A, B>(f: (_: A) => B) /* this could be a type Mapping */:
+    (_: TypedMap<A>) => TypedMap<B> =>
+        (coll: TypedMap<A>) => mapProperties(f)(Object.keys(coll), coll);
 
-    return (coll: TypedMap<A>) => mapProperties(f)(Object.keys(coll), coll);
-}
 
 
-export const filterMap = <T>(predicate: (_: T) => boolean): (_: TypedMap<T>) => TypedMap<T> => {
-
-    return (o: TypedMap<T>) => Object.keys(o).reduce((acc: TypedMap<T>, key: string|number) => {
-        if (predicate(o[key])) acc[key] = o[key];
-        return acc;
-    }, {});
-};
+export const filterMap = <T>(predicate: Predicate<T>): Transformation<TypedMap<T>> =>
+    (o: TypedMap<T>) =>
+        Object
+            .keys(o)
+            .reduce((acc: TypedMap<T>, key: string|number) => {
+                if (predicate(o[key])) acc[key] = o[key];
+                return acc;
+            }, {});
