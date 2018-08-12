b5e8572932be1dfcd20e18522cc4f385a004b238
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-12 17:35:43 +0200

    Implement flow in terms of compose

--- src/flow.ts
+++ src/flow.ts
@@ -1,10 +1,10 @@
-import {ArrayTransformation, Transformation} from './types';
+import {Transformation} from './types';
 
 // A flow is a transformation from Array of A to Array of A, consisting
 // of n transformation steps, where n >= 0.
 
 export const flow = <T>(t: T, ...transformations: Array<Transformation<T>>) =>
-    transformations.reduce((acc, transformation) => transformation(acc), t);
+    compose(...transformations)(t);
 
 
 export const flowP = <T>(...transformations: Array<Transformation<T>>) =>
@@ -14,3 +14,6 @@ export const flowP = <T>(...transformations: Array<Transformation<T>>) =>
 
 export const compose = <T>(...transformations: Array<Transformation<T>>) => (t: T)  =>
     transformations.reduce((acc, transformation) => transformation(acc), t);
+
+
+// TODO add 'partial' function
