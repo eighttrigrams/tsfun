2e23f9a2396d826eb160521d34fa082e3d4e353b
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-12 17:18:34 +0200

    Use standard formatting

--- test/flow.spec.ts
+++ test/flow.spec.ts
@@ -11,62 +11,68 @@ export function main() {
 
         it('flow', () =>
             expect(
-                flow(
-                    [5,4],
-                    takeWhile(biggerThan(4))
-                )
-            ).toEqual([5]));
+
+                flow([5,4],
+                    takeWhile(biggerThan(4))))
+
+            .toEqual([5]));
 
 
         it('flow - no steps', () =>
             expect(
+
                 flow(
-                    [5,6]
-                )
-            ).toEqual([5,6]));
+                    [5,6]))
+
+            .toEqual([5,6]));
 
 
         it('flowP', () =>
             expect(
-                flowP(
-                    take(1)
-                )([5, 6])
-            ).toEqual([5]));
+
+                flowP(take(1))([5, 6]))
+
+            .toEqual([5]));
 
 
         it('flowP - nest', () =>
             expect(
+
                 flow(
                     [5, 6],
                     flowP(
                         flowP(
-                            take(1)
-                        )
-                    )
-                )).toEqual([5]));
+                            take(1)))))
+
+            .toEqual([5]));
 
 
         it('reverse ', () =>
             expect(
-                reverse([1, 3])
-            ).toEqual(([3, 1])));
+
+                reverse([1, 3]))
+
+            .toEqual(([3, 1])));
 
 
         it('dropWhile', () =>
             expect(
+
                 flow(
                     [7, 9, 10, 13, 21, 20],
                     dropWhile(smallerThan(20)),
-                    reverse
-                ))
+                    reverse))
+
                 .toEqual([20, 21]));
 
 
         it('with objects', () =>
             expect(
+
                 flow(
                     {a: 1, b: 2},
                     uniteMap({c: 3})))
+
                 .toEqual({a: 1, b: 2, c: 3}));
     });
 }
