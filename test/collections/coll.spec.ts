019132973b3bc75d482691ad3e892925e6244909
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2018-08-12 14:53:04 +0200

    Use default formatting

--- test/collections/coll.spec.ts
+++ test/collections/coll.spec.ts
@@ -4,23 +4,25 @@ export function main() {
 
     describe('General/Collection', () => {
 
+
         it('copy - array',() =>
-            expect(copy([2,4])).toEqual([2, 4]));
+            expect(
 
+                copy([2,4]))
 
-        it('copy - array, retain instance',() => {
-            const instance = {a: 'hey'};
-            expect(copy([instance, 4])[0])
-                .toBe(instance)
-        });
+                .toEqual([2, 4]));
 
 
         it('copy - object, keys mixed numbers and strings',() =>
-            expect(copy({a: 1, 2: 2}))
+            expect(
+
+                copy({a: 1, 2: 2}))
+
                 .toEqual({a: 1, 2: 2}));
 
 
         it('copy - object, retain instance',() => {
+
             const instance = {a: 'hey'};
             expect(
                 copy({a: 1, 2: instance})[2])
@@ -29,7 +31,19 @@ export function main() {
 
 
         it('copy - retain existent keys with undefined properties',() =>
-            expect(copy({a: 1, 2: undefined}))
+
+            expect(
+
+                copy({a: 1, 2: undefined}))
+
                 .toEqual({a: 1, 2: undefined}));
+
+
+        it('copy - array, retain instance',() => {
+
+            const instance = {a: 'hey'};
+            expect(copy([instance, 4])[0])
+                .toBe(instance)
+        });
     });
 }
