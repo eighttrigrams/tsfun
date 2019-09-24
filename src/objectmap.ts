bc7707176c07366a7cd68be0838ead1b5677be6b
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2019-09-24 18:17:25 +0100

    Add typing for keysAndValues result

--- src/objectmap.ts
+++ src/objectmap.ts
@@ -3,7 +3,7 @@ import {zip} from "./arraylist";
 
 export const lookup = <A>(oc: ObjectCollection<A>) => (targetId: string): A => oc[targetId];
 
-export function keysAndValues<T>(o: ObjectCollection<T>) {
+export function keysAndValues<T>(o: ObjectCollection<T>): Array<[string, T]> {
 
-    return zip(Object.keys(o))(Object.values(o));
+    return zip(Object.keys(o))(Object.values(o)) as Array<[string, T]>;
 }
