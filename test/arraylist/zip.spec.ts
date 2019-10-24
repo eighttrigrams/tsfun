75b495efff80d53b09b17c95501f1ae7b3d29d77
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-10-24 19:21:28 +0100

    Add test

--- test/arraylist/zip.spec.ts
+++ test/arraylist/zip.spec.ts
@@ -3,7 +3,14 @@ import {zip} from "../../src/arraylist";
 describe('zip', () => {
 
 
-    it('zip', () => expect(
+    it('base case', () => expect(
+
+        zip([1, 2])(['a', 'b'])
+
+    ).toEqual([[1, 'a'], [2, 'b']]));
+
+
+    it('one list longer than the other', () => expect(
 
         zip([1, 2])(['a', 'b', 'c'])
 
