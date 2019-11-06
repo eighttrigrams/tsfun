b9fe54f9f3e52bdf1f4139c97695e6c2e92f4be8
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-11-06 11:46:12 +0000

    Add test

--- test/associative/dissoc.spec.ts
+++ test/associative/dissoc.spec.ts
@@ -17,4 +17,16 @@ describe('dissoc', () => {
             dissoc('b')({a: 3, b: 7})
 
         ).toEqual({a: 3}));
+
+
+    it('instances', () => {
+
+        const instance = {a: 3};
+        const originalColl = [4, instance as any, 7, 9];
+        const resultColl = dissoc(1)(originalColl);
+
+        expect(resultColl).not.toBe(originalColl);
+        expect(resultColl).toEqual([4, 7, 9]);
+        expect(instance).toEqual({a: 3});
+    });
 });
