a62e70ffc784bb8f2db66d9e6f5ee7b93d5c7b32
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2019-11-06 11:41:50 +0000

    Improve test and add doc

--- test/associative/copy.spec.ts
+++ test/associative/copy.spec.ts
@@ -1,9 +1,12 @@
 import {copy} from '../../src/associative';
 
 
+/**
+ * `copy` creates a shallow copy of an associative collection.
+ */
 describe('copy', () => {
 
-    it('copy - array',() =>
+    it('array',() =>
         expect(
 
             copy([2,4]))
@@ -11,11 +14,25 @@ describe('copy', () => {
             .toEqual([2, 4]));
 
 
-    it('copy - array, retain instance',() => {
+    it('array instance new, instance value same',() => {
 
         const instance = {a: 'hey'};
-        expect(copy([instance, 4])[0])
-            .toBe(instance)
+        const originalColl = [instance, 4];
+        const copiedColl = copy(originalColl);
+
+        expect(copiedColl).not.toBe(originalColl);
+        expect(copiedColl[0]).toBe(instance);
+    });
+
+
+    it('object instance new, instance value same',() => {
+
+        const instance = {a: 'hey'};
+        const originalColl = {a: instance, b: 4};
+        const copiedColl = copy(originalColl);
+
+        expect(copiedColl).not.toBe(originalColl);
+        expect(copiedColl['a']).toBe(instance);
     });
 
 
@@ -25,15 +42,6 @@ describe('copy', () => {
             copy({a: 1, 2: 2}))
 
             .toEqual({a: 1, 2: 2}));
-
-
-    it('copy - object, retain instance',() => {
-
-        const instance = {a:  'hey'};
-        expect(
-            copy({a: 1, 2: instance})[2])
-            .toBe(instance)
-    });
 
 
     it('copy - retain existent keys with undefined properties',() =>
