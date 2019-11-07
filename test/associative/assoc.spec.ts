538a2e7e3be726f4d654f3faf2e637120c795e75
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2019-11-07 19:11:35 +0100

    Add test

--- test/associative/assoc.spec.ts
+++ test/associative/assoc.spec.ts
@@ -17,4 +17,12 @@ describe('assoc', () => {
             assoc('b', 8)({a: 3, b: 7})
 
         ).toEqual({a: 3, b: 8}));
+
+
+    it('array - ignore undefined indices', () => {
+
+        const result = assoc(3, 8)([11, 12]);
+        expect(result[3]).toEqual(8);
+        expect(result[2]).toBeUndefined();
+    });
 });
