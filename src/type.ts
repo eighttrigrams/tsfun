2f92ca41842a8aea307fd7980fa2eeb1e623d766
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-03-11 16:56:43 +0000

    Improve typing; add comment

--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,7 @@
 // ------------ @author Daniel de Oliveira -----------------
 
 
-export interface Map<T> {[prop: string]: T}
+export interface Map<T = any> {[prop: string]: T}
 
 
 export type Mapping<A = any, B = A> = (_: A) => B;
@@ -22,16 +22,13 @@ export type Right<T = any> = [undefined, T];
 export type Either<T1 = any, T2 = any> = Left<T1>|Right<T2>;
 
 
-export type Just<T> = [T];
+export type Just<T = any> = [T];
 
 
 export type Nothing = [];
 
 
-export type Maybe<T> = Nothing | Just<T>;
-
-
-export type Fallible<T> = Either<any, T> | Maybe<T>;
+export type Maybe<T = any> = Nothing | Just<T>;
 
 
 export type Associative<T = any> = Map<T> | Array<T>;
@@ -43,13 +40,16 @@ export type List<T = any> = Array<T> | string;
 export type Collection<T = any> = Associative<T> | List<T>;
 
 
-export type Predicate<A> = (_: A) => boolean;
+export type Predicate<A = any> = (_: A) => boolean;
 
+
+
+//////// Internal or experimental ///////////////
 
 export type Comparator = <A, B>(_: A) => Predicate<B>;
 
 
-
+export type Fallible<T> = Either<any, T> | Maybe<T>;
 
 
 export type ComparatorProducer = (_: Comparator) => Comparator;
