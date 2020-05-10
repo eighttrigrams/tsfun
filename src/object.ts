a3f716959c73b67b93f4f54abf02e43c8d8dc0fa
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-05-10 21:01:27 +0100

    Remove semicolons

--- src/object.ts
+++ src/object.ts
@@ -3,7 +3,7 @@ import {copy} from "./collection";
 
 export function assoc<T, K extends keyof T>(key: keyof T, val: T[K], o: T): T {
 
-        const c: any = copy(o as any);
-        c[key] = val;
-        return c as T;
-    }
+    const c: any = copy(o as any)
+    c[key] = val
+    return c as T
+}
