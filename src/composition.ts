e1be2a049dcc5b9dce0b91798992d953e9972a1c
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-05-11 01:38:26 +0100

    Remove semicolons

--- src/composition.ts
+++ src/composition.ts
@@ -1,19 +1,19 @@
-import {identity} from './core';
-import {Either, Fallible, Mapping, Maybe, Pair, Predicate} from './type';
-import {isEither, isFailure, isFunction, isMaybe, isSuccess} from './predicate';
-import {first, rest, reverse} from './list';
-import {success, getSuccess, left, just, right} from './tuple';
+import {identity} from './core'
+import {Either, Fallible, Mapping, Maybe, Pair, Predicate} from './type'
+import {isEither, isFailure, isFunction, isMaybe, isSuccess} from './predicate'
+import {first, rest} from './list'
+import {success, getSuccess, left, just, right} from './tuple'
 
 
 const composition = <T = any>(t: any, ...transformations: Array<Function>) =>
-    compose(...transformations)(t) as T;
+    compose(...transformations)(t) as T
 
 
-export const flow = composition;
+export const flow = composition
 
 
 export const compose = (...transformations: Array<Function>) => (t: any)  =>
-    transformations.reduce((acc, transformation) => transformation(acc), t) as any;
+    transformations.reduce((acc, transformation) => transformation(acc), t) as any
 
 
 export function cond<A, B, C>(
@@ -40,34 +40,34 @@ export function val<A>(v: A) { return () => v as A }
 
 export function throws(e1?: any) {
 
-    return (e2?: any): any => { throw e1 ? e1 : e2 };
+    return (e2?: any): any => { throw e1 ? e1 : e2 }
 }
 
 
 export function collect<T>(...p: Array<T>) {
 
-    return p;
+    return p
 }
 
 
 export function mcompose<T, R>(...fs: Array<(x: T, ...xs: Array<T>) => Either<any, T>>)
-    : (seed: Either<any, T>) => Either<any, R>;
+    : (seed: Either<any, T>) => Either<any, R>
 export function mcompose<T, R>(...fs: Array<(x: T, ...xs: Array<T>) => Maybe<T>>)
-    : (seed: Maybe<T>) => Maybe<R>;
+    : (seed: Maybe<T>) => Maybe<R>
 export function mcompose<T, R>(...fs: Array<(x: T, ...xs: Array<T>) => Either<any, T>|Maybe<T>>)
     : (seed: Either<any, T>|Maybe<T>) => Maybe<R>|Either<any, R> {
 
     return (seed: Maybe<T>|Either<any, T>) => {
-        if (isFailure(seed)) return seed as any;
+        if (isFailure(seed)) return seed as any
 
-        let results = [getSuccess(seed)] as Array<T>;
+        let results = [getSuccess(seed)] as Array<T>
         for (let f of fs) {
 
-            const res = f(first(results) as T, ...rest(results));
-            if (isFailure(res)) return res as any;
-            results = [getSuccess(res)].concat(results);
+            const res = f(first(results) as T, ...rest(results))
+            if (isFailure(res)) return res as any
+            results = [getSuccess(res)].concat(results)
         }
-        return convert(first(results), seed) as any;
+        return convert(first(results), seed) as any
     }
 }
 
@@ -77,7 +77,7 @@ export function convert<T>(what: any, basedOn: Fallible<T>): any {
     if (!isEither(basedOn) && !isMaybe(basedOn)) throw 'illegal argument - basedOn is neither Maybe nor Either';
     return isEither(basedOn)
         ? success(what)
-        : just(what);
+        : just(what)
 }
 
 
@@ -88,7 +88,7 @@ export function mmatch<T, R>(onSuccess: (x: T) => R,
 
         return isSuccess(m)
             ? onSuccess((m as any)[0])
-            : onFailure();
+            : onFailure()
     }
 }
 
@@ -99,19 +99,19 @@ export function conds(...cs: Array<Pair>) {
 
         for (let c of cs) {
 
-            const rightC = right(c); // avoid calling it twice
-            const r = () => isFunction(rightC) ? rightC(what) : rightC;
+            const rightC = right(c) // avoid calling it twice
+            const r = () => isFunction(rightC) ? rightC(what) : rightC
 
             const leftC = left(c); // same
             if (isFunction(leftC)) {
-                if (leftC(what)) return r();
+                if (leftC(what)) return r()
             } else {
-                if (leftC === what) return r();
+                if (leftC === what) return r()
             }
         }
-        throw 'case exception - try using \'otherwise\' in conds';
+        throw 'case exception - try using \'otherwise\' in conds'
     }
 }
 
 
-export const otherwise = val(true);
+export const otherwise = val(true)
