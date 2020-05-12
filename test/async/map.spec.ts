d8e512405d3e254fc2f6709204c23275e29a2b01
danielmarreirosdeoliveira <danielmarreirosdeoliveira@googlemail.com>
2020-05-12 22:26:42 +0100

    Improve documentation

--- test/async/map.spec.ts
+++ test/async/map.spec.ts
@@ -1,32 +1,36 @@
-import {map as asyncMap} from '../../src/async';
+import {map as asyncMap /* use an alias if you want to disambiguate */} from '../../src/async';
 
 /**
  * tsfun | async/map
  */
 describe('async/map', () => {
 
-    const asyncTimes2 =
-        (_: number) => Promise.resolve(_ * 2)
-    const asyncTimes2WithTimeout =
-        (_: number) => new Promise<any>(resolve => setTimeout(() => resolve(_ * 2), 50))
+    it('async map over associative', async done => {
 
+        // mapping as usual, but with an async mapping function
+        expect(
 
-    it('array', async done => {
+            await asyncMap(delayedTimes2, [1, 2])
 
-        expect(await asyncMap(asyncTimes2WithTimeout)([1, 2])).toEqual([2, 4])
-        expect(await asyncMap(asyncTimes2WithTimeout, [1, 2])).toEqual([2, 4])
+        ).toEqual([2, 4])
+
+        // works for Map, abstracting over Associative
+        expect(
+
+            await asyncMap(delayedTimes2, {a: 1, b: 2})
+
+        ).toEqual({a: 2, b: 4})
         done()
     });
 
 
-    it('object', async done => {
+    it('multiple param lists for use in composition', async done => {
 
-        expect(
-
-            await asyncMap(asyncTimes2)({a: 1, b: 2}))
-
-            .toEqual({a: 2, b: 4})
-
+        expect(await asyncMap(delayedTimes2)([1, 2])).toEqual([2, 4])
         done()
     })
+
+
+    const delayedTimes2 =
+        (_: number) => new Promise<any>(resolve => setTimeout(() => resolve(_ * 2), 50))
 })
