294f3979556c3ff623a532f8d6d517f1d21ac1ff
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-05-15 00:10:55 +0100

    Remove semicolons and organize imports

--- src/array.ts
+++ src/array.ts
@@ -15,18 +15,17 @@ import {
     dropWhile as listDropWhile,
     takeWhile as listTakeWhile,
     takeRightWhile as listTakeRightWhile,
-    dropRightWhile as listDropRightWhile, zip, prepend
+    dropRightWhile as listDropRightWhile, zip,
 } from './list'
 import {identity, uncurry2} from './core'
 import {Associative, Mapping, Pair, Predicate} from './type'
-import {values, map as mapAsc} from './associative';
+import {values, map as mapAsc} from './associative'
 import {
     filter as filterColl, 
     remove as removeColl,
     separate as separateColl
 } from './collection'
-import {compose} from "./composition";
-import {pair} from "./tuple";
+import {pair} from './tuple'
 
 
 export const flatMap = <A, B>(f: (_: A) => Array<B>) =>
@@ -117,21 +116,21 @@ export function map<A, B>(...args: any[]): any {
 
 
 // TODO allow deeper levels to get collapsed in associative, if they all are arrays. if not, then stop at the level, it cannot get further collapsed
-// leftover: export function flatten<T>(as: Associative<T>): Array<T>;
+// leftover: export function flatten<T>(as: Associative<T>): Array<T>
 
-export function flatten<U, T extends Array<U>>(as: Array<T>): T;
-export function flatten<U, T extends Array<U>>(depth: 1, as: Array<T>): T;
-export function flatten<U, T extends Array<U>>(as: Array<T>, depth: 1): T;
+export function flatten<U, T extends Array<U>>(as: Array<T>): T
+export function flatten<U, T extends Array<U>>(depth: 1, as: Array<T>): T
+export function flatten<U, T extends Array<U>>(as: Array<T>, depth: 1): T
 export function flatten<U, T extends Array<U>, K extends Array<T>>(depth: 2, as: Array<K>): T
 export function flatten<U, T extends Array<U>, K extends Array<T>>(as: Array<K>, depth: 2): T
 export function flatten<U, T extends Array<U>, K extends Array<T>, V extends Array<K>>(depth: 3, as: Array<V>): T
 export function flatten<U, T extends Array<U>, K extends Array<T>, V extends Array<K>>(as: Array<V>, depth: 3): T
-export function flatten<U, T extends Array<U>>(as: Array<T>, depth: number): T;
-export function flatten<U, T extends Array<U>>(depth: number, as: Array<T>): T;
-export function flatten<T>(as: Array<T>, depth: number): Array<unknown>;
-export function flatten(depth: void): <U, T extends Array<U>>(as: Array<T>) => T;
-export function flatten(depth: 1): <U, T extends Array<U>>(as: Array<T>) => T;
-export function flatten(depth: number): <T,R>(as: Array<T>) => Array<R>;
+export function flatten<U, T extends Array<U>>(as: Array<T>, depth: number): T
+export function flatten<U, T extends Array<U>>(depth: number, as: Array<T>): T
+export function flatten<T>(as: Array<T>, depth: number): Array<unknown>
+export function flatten(depth: void): <U, T extends Array<U>>(as: Array<T>) => T
+export function flatten(depth: 1): <U, T extends Array<U>>(as: Array<T>) => T
+export function flatten(depth: number): <T,R>(as: Array<T>) => Array<R>
 export function flatten(p1: any, ...p2: any[]): any {
 
     if (p2.length > 1) throw 'illegal arguments - in \'flatten\''
@@ -142,7 +141,7 @@ export function flatten(p1: any, ...p2: any[]): any {
         )) throw 'illegal arguments - in \'flatten\''
     }
 
-    const _flatten = flatMap(identity as any) as any;
+    const _flatten = flatMap(identity as any) as any
 
     const inner = (num: number) =>
         (as: Associative) => {
