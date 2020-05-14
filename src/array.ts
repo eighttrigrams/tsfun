03a39e2e332272120d7bf840ace8f9308d16b75c
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-05-15 00:07:28 +0100

    Improve zipWith

--- src/array.ts
+++ src/array.ts
@@ -15,9 +15,9 @@ import {
     dropWhile as listDropWhile,
     takeWhile as listTakeWhile,
     takeRightWhile as listTakeRightWhile,
-    dropRightWhile as listDropRightWhile
+    dropRightWhile as listDropRightWhile, zip, prepend
 } from './list'
-import {identity} from './core'
+import {identity, uncurry2} from './core'
 import {Associative, Mapping, Pair, Predicate} from './type'
 import {values, map as mapAsc} from './associative';
 import {
@@ -25,6 +25,8 @@ import {
     remove as removeColl,
     separate as separateColl
 } from './collection'
+import {compose} from "./composition";
+import {pair} from "./tuple";
 
 
 export const flatMap = <A, B>(f: (_: A) => Array<B>) =>
@@ -66,26 +68,26 @@ export function range(a: number, b?: number, stepSize: number = 1): number[] {
 }
 
 
-export function zipWith<A,B,C> (f: (a: A, b: B) => C, as: Array<A>) {
 
-    return (bs: Array<B>): Array<C> => {
+export function zipWith<A,B,C> (f: (a: A, b: B) => C, as: Array<A>): (as2: Array<B>) => Array<C>
+export function zipWith<A,B,C> (f: (a: A, b: B) => C, as: A[], bs: B[]): Array<C>
+export function zipWith<A,B,C,D>(f: (a: A, b: B, c: C) => D, as: A[], bs: B[], cs: C[]): Array<D>
+export function zipWith<A>(f: (...a) => A, ...as: any[][]): A[]
+export function zipWith<A,B,C> (f, ...aas): any {
 
-        const minimumLength = Math.min(as.length, bs.length)
-        const _as = take(minimumLength)(as)
-        const _bs = take(minimumLength)(bs)
+    const inner = aas => reduce1(uncurry2(zip))(aas)
+        .map(flatten(aas.length-1))
+        .map(_ => f.apply(null, _))
 
-        const zipped: Array<C> = []
-        for (let i = 0; i < minimumLength; i++) {
-            zipped.push(f((_as as any)[i] as A, (_bs as any)[i] as B))
-        }
-        return zipped
-    }
+    return aas.length === 1
+        ? bs => inner(pair(aas[0],bs))
+        : inner(aas)
 }
 
 
 export function reduce1<T>(f: (b: T, t: T, i?: number) => T) {
 
-    return (ts: Array<T>): T => {
+    return (ts: T[]): T => {
 
         if (isArray(ts) && !isEmpty(ts)) {
 
--- test/array/zip_with.spec.ts
+++ test/array/zip_with.spec.ts
@@ -1,14 +1,41 @@
 import {zipWith} from "../../src/array";
 
+/**
+ * tsfun | zipWith
+ */
 describe('zipWith', () => {
 
+    it('multiple argument lists', () => expect(
 
-    it('zipWith', () => expect(
-
-        zipWith(
-            (x: number, y: number) => x + y,
-            [1, 2])
-        ([3, 4, 5])
+        zipWith((x: number, y: number) => x + y, [1, 2])([3, 4, 5])
 
     ).toEqual([4, 6]))
+
+
+    it('single argument list - 2', () => expect(
+
+        zipWith((x: number, y: number) => x + y, [1, 2], [3, 4, 5])
+
+    ).toEqual([4, 6]))
+
+
+    it('3 arguments', () => expect(
+
+        zipWith((x: number, y: number, z: number) => x + y + z, [1, 2], [3, 4, 5], [1, 1, 1])
+
+    ).toEqual([5, 7]))
+
+
+    it('4 arguments or more', () => expect(
+
+        zipWith((x: number, y: number, z: number, a: number) => x + y + z + a, [1, 2], [3, 4, 5], [1, 1, 1], [1, 1, 1])
+
+    ).toEqual([6, 8]))
+
+
+    it('5 arguments', () => expect(
+
+        zipWith((x: number, y: number, z: number, a: number, b: number) => x + y + z + a + b, [1, 2], [3, 4, 5], [1, 1, 1], [1, 1, 1], [1])
+
+    ).toEqual([7]))
 });
