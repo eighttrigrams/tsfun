175ab40a33e72631c67f21c766f88a4a78031a79
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2020-07-03 21:01:44 +0100

    Add spread function

--- test/array/zip.spec.ts
+++ test/array/zip.spec.ts
@@ -1,4 +1,5 @@
 import {zip} from "../../src/array";
+import {collect, flow} from '../../src/composition';
 
 /**
  * tsfun | zip
@@ -36,6 +37,28 @@ describe('zip', () => {
         expect(
 
             zip(...[[1,2], [3,4]])
+
+        ).toEqual([[1,3],[2,4]])
+    );
+
+
+    // TODO move to composition
+    function spread<A,B>(f: (_: Array<Array<A>>) => B) {
+
+        return (as: Array<Array<A>>): B => {
+
+            return (f as any)(...as)
+        }
+    }
+    it('spread - typing', () => {
+
+        const result: Array<Array<number>> = flow([[1,2],[3,4]], spread(zip));
+    });
+
+    it('composition', () =>
+        expect(
+
+            flow([[1,2],[3,4]], spread(zip))
 
         ).toEqual([[1,3],[2,4]])
     );
