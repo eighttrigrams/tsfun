c83fa4c6c45086ca98b5e3f3736c7586b4c7ef14
Daniel de Oliveira <danielmarreirosdeoliveira@googlemail.com>
2021-03-29 02:53:58 +0200

    Format file

--- test/predicate/is_undefined.spec.ts
+++ test/predicate/is_undefined.spec.ts
@@ -1,13 +1,15 @@
-import {isUndefined} from "../../src/predicate";
+import {isUndefined} from '../../src/predicate'
 
 
+/**
+ * tsfun | isUndefined
+ */
 describe('isUndefined', () => {
-
 
     it('isFalse',() =>
         expect(
 
             isUndefined(undefined))
 
-            .toEqual(true));
-});
+            .toEqual(true))
+})
